c27d087c9a93adef4291a1031b92db5b0e88234d
thierry <thierry.henrio@gmail.com>
2010-07-17 00:37:27 +0200

    sign in scenario requires a user step

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl'
+require 'debugger'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
