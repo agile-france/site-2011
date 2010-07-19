ad0b4ed8708a667ea3cd62ee30f9cd046841f624
thierry <thierry.henrio@gmail.com>
2010-07-19 09:45:41 +0200

    localization triangulation

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,17 +1,30 @@
 #encoding: utf-8
 require 'spec_helper'
-#require 'i18n'
+require 'i18n'
 
 describe "home/index.html.haml" do
   describe ', with fr locale' do
     before do
- #     I18n.default_locale = :fr
+      I18n.default_locale = :fr
       render
     end
 
     it 'should have h1. Conférence Agile France' do
       rendered.should have_tag('h3') do |h3|
         h3.should contain 'Conférence Agile France'
+      end
+    end
+  end
+
+  describe ', with en locale' do
+    before do
+      I18n.default_locale = :en
+      render
+    end
+
+    it 'should have h1. Conference Agile France' do
+      rendered.should have_tag('h3') do |h3|
+        h3.should contain 'Conference Agile France'
       end
     end
   end
