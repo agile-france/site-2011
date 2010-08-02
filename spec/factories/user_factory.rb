e8e78a092aa26d2e20a41c21e58820c6b141eabc
thierry <thierry.henrio@gmail.com>
2010-08-03 00:02:09 +0200

    describe full_name

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,4 +1,6 @@
 Factory.define :user do |u|
   u.email 'john@doe.com'
   u.password  'git rocks'
+  u.first_name 'john'
+  u.last_name 'doe'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 describe User do
   it {should validate_presence_of :first_name}
   it {should validate_presence_of :last_name}
+
+  describe 'full_name' do
+    it 'should return John Doe' do
+      Factory.create(:user).full_name.should == 'John Doe'
+    end
+  end
 end
