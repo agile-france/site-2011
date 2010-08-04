285e0753ce321c6791de68a1a16fcdd8aa492c5c
thierry <thierry.henrio@gmail.com>
2010-08-05 00:24:46 +0200

    add mark for required fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def mark
+    '*'
+  end
+  
+  def required?(object, attribute)
+    object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator  
+  end
 end
