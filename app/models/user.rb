6333d8c53444919eb53947cc31064bd18b256e5e
thierry <thierry.henrio@gmail.com>
2010-08-18 23:44:27 +0200

    user has many sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
-  # names are capitalized
+
+  has_many :sessions
+
+  # names are capitalized before validation
   before_validation do
     [:first_name, :last_name].each do |symbol|
       self.send(symbol).capitalize! if attribute_present? symbol
