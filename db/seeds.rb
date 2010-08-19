6586c0541b84323a6a9f2c0c66dfe4f7d67721b1
thierry <thierry.henrio@gmail.com>
2010-08-20 00:57:47 +0200

    ?? have a handy session in seeds ??

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# ugly seeding, no environment, monolithic
+Conference.create(:name => 'Conférence Agile France', :edition => 2011)
