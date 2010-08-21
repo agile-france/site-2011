73d3c37c081c78b0200a659f02db9833a2dd8c93
thierry <thierry.henrio@gmail.com>
2010-08-22 01:38:46 +0200

    so dumb to have forgotten #encoding: utf-8 and have french in text :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # ugly seeding, no environment, monolithic
 Conference.create(:name => 'Conférence Agile France', :edition => 2011)
