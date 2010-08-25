5aa8b1a96e3b2a5b81ee829fd89ac203ae8ecd1f
thierry <thierry.henrio@gmail.com>
2010-08-25 23:13:11 +0200

    spec signed in user can do more

--- spec/controllers/conference/sessions_controller_spec.rb
+++ spec/controllers/conference/sessions_controller_spec.rb
@@ -1,17 +1,37 @@
+#encoding: utf-8
 require 'spec_helper'
 
 describe Conference::SessionsController do
-  describe "GET new" do
-    it "should be successful" do
-      get :new
-      response.should be_success
+  include Devise::TestHelpers
+
+  describe ', with a signed in user' do
+    before do
+      @john = Factory.create(:user)
+      sign_in @john
+    end
+
+    describe ", GET new" do
+      it "should be successful" do
+        get :new
+        response.should be_success
+      end
+    end
+
+    describe ", POST create" do
+      it "should be successful" do
+        post :create
+        response.should redirect_to root_path
+      end
     end
   end
 
-  describe "POST create" do
-    it "should be successful" do
-      post :create
-      response.should redirect_to root_path
+  describe ', with a signed off user' do
+    describe ", GET new" do
+      it "should not be granted and have a flash notice" do
+        get :new
+        response.should redirect_to root_path
+        flash[:notice].should == 'Vous devez etre connecté'
+      end
     end
   end
 end
