6c5515eb4d2044261795ba2ca79dc117cccb8f08
thierry <thierry.henrio@gmail.com>
2010-09-17 17:26:57 +0200

    spec edit view (this modularized, nested model is a pain in the ass) went to a bad option for : 'updating title does not work' learned at least how PUT is done

---
+++ spec/views/party/sessions/edit.html.haml_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "party/sessions/edit.html.haml" do
+  before do
+    @conference = Factory(:conference)
+    @session = Factory(:session, :id => 123, :title => 'courage', :conference => @conference)
+
+    render :template => 'party/sessions/edit.html.haml',
+           :locals => {:session => @session}
+  end
+  it 'should use PUT to /conferences/1/sessions/123' do
+    rendered.should have_tag('form[action="/conferences/1/sessions/123"]')
+    # PUT is POST+hidden field (see http://guides.rubyonrails.org/form_helpers.html#how-do-forms-with-put-or-delete-methods-work)
+  end
+end
