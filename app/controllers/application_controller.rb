3235a7ce34dd21dd4debc20697b2724496cd33ed
thierry <thierry.henrio@gmail.com>
2010-09-20 17:02:28 +0200

    rescue authorization failures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
     flash[:error] = error.message
     redirect_to root_path
   end
+
+  rescue_from Failures::AccessDenied do |failure|
+    flash[:error] = failure.message
+    redirect_to :back
+  end
 end
