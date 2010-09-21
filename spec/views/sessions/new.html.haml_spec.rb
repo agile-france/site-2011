faf0e41dcc6390667b26e1b307b759ce30046a78
thierry <thierry.henrio@gmail.com>
2010-09-21 15:34:54 +0200

    house cleaning

--- spec/views/sessions/new.html.haml_spec.rb
+++ spec/views/sessions/new.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-  describe "sessions/new.html.haml" do
+describe "sessions/new.html.haml" do
   before do
     assign(:conference, Factory(:conference))
     assign(:session, Factory(:session))
