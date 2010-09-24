002a11f154580f15758add86c0277d1107d8050b
thierry <thierry.henrio@gmail.com>
2010-09-24 11:52:24 +0200

    can navigate to session

--- spec/views/conferences/show.html.haml_spec.rb
+++ spec/views/conferences/show.html.haml_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe "conferences/show.html.haml" do
   before do
-    @conference = Factory(:conference)
-    assign(:conference, @conference)
+    @xp = Factory(:conference, :id => 3)
+    @kent = Factory(:user, :first_name => 'kent', :last_name => 'beck')
+    @kent.propose(Session.new({:id => 6, :title => 'explained'}), @xp)
+    assign(:conference, @xp)
     render
   end
 
   it 'should have a link to propose a new session' do
-    rendered.should have_tag('a[href="/conferences/1/sessions/new"]',
+    rendered.should have_tag('a[href="/conferences/3/sessions/new"]',
                              t('party.session.new?'))
   end
 
   it 'should have a nice title' do
     rendered.should have_tag('h1')
+  end
+  
+  it 'should have a link to session' do
+    rendered.should have_tag('a[href="/sessions/6"]') 
   end
 end
