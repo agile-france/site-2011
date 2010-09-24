5cdd3e74b36dd6a5584bef835c254754e02c8835
thierry <thierry.henrio@gmail.com>
2010-09-24 21:13:57 +0200

    ouch seeds were old

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # ugly seeding, no environment, monolithic
-Party::Conference.create(:name => 'agile-france', :edition => 2011)
+Conference.create(:name => 'agile-france', :edition => 2011)
