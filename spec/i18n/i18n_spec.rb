d5dcecfa3c42aa6357934012580925d6afc8e11c
thierry <thierry.henrio@gmail.com>
2010-09-27 21:00:18 +0200

    more behaviours for i18n

--- spec/i18n/i18n_spec.rb
+++ spec/i18n/i18n_spec.rb
@@ -5,9 +5,31 @@ describe 'i18n configuration' do
     I18n.default_locale.should == :fr
   end
   
+  it 'has fr locale' do
+    I18n.locale.should == :fr
+  end
+  
   it 'should fallback to :en when translation is missing' do
     I18n.should respond_to(:fallbacks)
     I18n.fallbacks[:fr].should == [:fr, :en]
   end
+  
+  describe 'translate message' do
+    before do
+      I18n.backend.store_translations(:fr, {:cheese => 'fromage'})
+      I18n.backend.store_translations(:en, {:choose => 'choose'})
+    end
     
+    it 'uses available :fr translation' do
+      I18n.translate(:cheese).should == 'fromage'
+    end
+    
+    it 'look for :en fallback translation' do
+      I18n.translate(:choose).should == 'choose'
+    end
+    
+    it 'raises when no translation' do
+      I18n.translate(:goose).should == 'choose'
+    end
+  end
 end
