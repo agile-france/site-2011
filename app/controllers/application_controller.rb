d2dd81cf945dc4915d43c39ba1833b0a4884ec76
thierry <thierry.henrio@gmail.com>
2010-09-28 15:21:19 +0200

    recognize :locale parameter in uri and set i18n accordingly done on each request (I18n.locale is ... global)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
 
+  prepend_before_filter :localize
+  def localize
+    user_locale = params['locale'].to_sym if params['locale']
+    I18n.locale = user_locale if user_locale
+    logger.debug("using locale #{I18n.locale}")
+  end
+
   rescue_from ActiveRecord::RecordNotFound do |error|
     flash[:error] = error.message
     redirect_to root_path
