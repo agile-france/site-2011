f95d9f2b3228874af3fab62115616cf9146f6909
thierry <thierry.henrio@gmail.com>
2010-10-14 19:16:05 +0200

    path is really /database

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,15 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'active_resource/railtie'
 
+# FIXME this is ugly
 if ENV['MONGOHQ_URL']
   require 'uri'
-  mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
-  ENV['MONGOID_HOST'] = mongo_uri.host
-  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
-  ENV['MONGOID_USERNAME'] = mongo_uri.user
-  ENV['MONGOID_PASSWORD'] = mongo_uri.password
-  ENV['MONGOID_DATABASE'] = mongo_uri.path
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  ENV['MONGOID_HOST'] = uri.host
+  ENV['MONGOID_PORT'] = uri.port.to_s
+  ENV['MONGOID_USERNAME'] = uri.user
+  ENV['MONGOID_PASSWORD'] = uri.password
+  ENV['MONGOID_DATABASE'] = uri.path[1..-1]
 end
 
 # If you have a Gemfile, require the gems listed there, including any gems
