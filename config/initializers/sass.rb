74e0118bdc91aae309546dfa95357ecb0b04b9a2
thierry <thierry.henrio@gmail.com>
2010-10-14 23:37:24 +0200

    cope with read only source tree http://blog.heroku.com/archives/2009/8/18/heroku_sass/

---
+++ config/initializers/sass.rb
@@ -0,0 +1,9 @@
+# source tree is read only on heroku (http://blog.heroku.com/archives/2009/8/18/heroku_sass/)
+# there are options
+# - add static middleware to rw directory (http://blog.siyelo.com/the-easiest-way-to-make-sasscompass-work-on-h)
+# that is retained option
+# - commit generated css (git pre-commit hook, client pre-push does not exist yet I believe)
+# like it tough it does cripple the source tree with duplication
+# there is something missing with deploying on heroku, build|generate as post push
+Sass::Plugin.options[:css_location] = 'tmp/stylesheets'
+Rails::Application.middleware.use Rack::Static, :root => 'tmp', :urls => ['/stylesheets']
