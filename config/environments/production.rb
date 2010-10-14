19d64aa369fb62d74ed8889dc956de5b7da94076
thierry <thierry.henrio@gmail.com>
2010-10-14 19:15:14 +0200

    serve assets configure deprecation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ ConferenceOnRails::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -40,7 +40,6 @@ ConferenceOnRails::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  # Deprecation
+  config.active_support.deprecation = :notify
 end
