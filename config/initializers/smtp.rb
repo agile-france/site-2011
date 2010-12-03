2ec4593f0952bc4021546f4661bd8829ac2ca32a
thierry <thierry.henrio@gmail.com>
2010-12-03 13:05:44 +0100

    use env

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true,
-  :user_name => 'thierry.henrio@gmail.com',
-  :password => "gas7roll"
+  :user_name => ENV['mailer.user_name'],
+  :password => ENV['mailer.password']
 }
 ActionMailer::Base.default_url_options[:host] = ENV['mailer.host_url_option']
 ActionMailer::Base.delivery_method = :smtp
