c70ec00b248f3b112ebdcb2196e8bd50db70f9ba
thierry <thierry.henrio@gmail.com>
2010-12-03 13:24:48 +0100

    onoe, this heroku stuff was not working again !!! self << roundhouse_kick!

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # define here what heroku config store in account
 # see http://tammersaleh.com/posts/managing-heroku-environment-variables-for-local-development
 # deploy with git, though keep private informations ...
-heroku = File.expand_path("../environments/heroku/#{Rails.env}", __FILE__)
-require heroku if File.exists?(heroku) 
+heroku = File.expand_path("environments/heroku/#{Rails.env}.rb", File.dirname(__FILE__))
+load heroku if File.exists?(heroku)
 
 # Initialize the rails application
 ConferenceOnRails::Application.initialize!
