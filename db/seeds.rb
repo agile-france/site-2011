55290ea4b4bf475096487fb8555acc59d085252c
thierry <thierry.henrio@gmail.com>
2010-12-14 17:37:47 +0100

    default conference seed is name: 'Agile France'

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # ugly seeding, no environment, monolithic
-agile_france_2011 = {:name => 'agile-france', :edition => 2011}
+agile_france_2011 = {:name => 'Agile France', :edition => 2011}
 if Conference.where(agile_france_2011).empty?
   c = Conference.create(agile_france_2011)
   puts "created #{c}"
