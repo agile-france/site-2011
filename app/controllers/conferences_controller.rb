e4b2158126dd2c6b37f5a4b650b6fe6ca4b70d5c
thierry <thierry.henrio@gmail.com>
2011-01-04 10:09:12 +0100

    paginate sessions of a conference

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -3,11 +3,17 @@ class ConferencesController < ApplicationController
 
   def index
     @conferences = Conference.all
-    respond_with @conferences
+    respond_with @conferences.paginate(pager_options)
   end
 
   def show
     @conference = Conference.find(params[:id])
-    respond_with @session
+    @sessions = @conference.sessions.paginate(pager_options)
+    respond_with @conference
   end
+  
+  private
+  def pager_options
+    {:page => params[:page], :per_page => params[:per_page] || 5}
+  end  
 end
