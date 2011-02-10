6bb40b3a3cdae796bb821a47d80ff41a3f99f4fc
thierry <thierry.henrio@gmail.com>
2011-02-10 22:21:20 +0100

    oops, there is a stench there to have initialiser depends on actual env level ...

--- config/initializers/barista_config.rb
+++ config/initializers/barista_config.rb
@@ -1,48 +1,50 @@
-# Configure barista.
-Barista.configure do |c|
+unless Rails.env.production?
+  # Configure barista.
+  Barista.configure do |c|
   
-  # Change the root to use app/scripts
-  # c.root = Rails.root.join("app", "scripts")
+    # Change the root to use app/scripts
+    # c.root = Rails.root.join("app", "scripts")
   
-  # Change the output root, causing Barista to compile into public/coffeescripts
-  # c.output_root = Rails.root.join("public", "coffeescripts")
+    # Change the output root, causing Barista to compile into public/coffeescripts
+    # c.output_root = Rails.root.join("public", "coffeescripts")
   
-  # Set the compiler
+    # Set the compiler
   
-  # Disable wrapping in a closure:
-  # c.no_wrap = true
-  # ... or ...
-  # c.no_wrap!
+    # Disable wrapping in a closure:
+    # c.no_wrap = true
+    # ... or ...
+    # c.no_wrap!
   
-  # Change the output root for a framework:
+    # Change the output root for a framework:
   
-  # c.change_output_prefix! 'framework-name', 'output-prefix'
+    # c.change_output_prefix! 'framework-name', 'output-prefix'
   
-  # or for all frameworks...
+    # or for all frameworks...
   
-  # c.each_framework do |framework|
-  #   c.change_output_prefix! framework.name, "vendor/#{framework.name}"
-  # end
+    # c.each_framework do |framework|
+    #   c.change_output_prefix! framework.name, "vendor/#{framework.name}"
+    # end
   
-  # or, prefix the path for the app files:
+    # or, prefix the path for the app files:
   
-  # c.change_output_prefix! :default, 'my-app-name'  
+    # c.change_output_prefix! :default, 'my-app-name'  
   
-  # or, hook into the compilation:
+    # or, hook into the compilation:
   
-  c.before_compilation   { |path|         puts "Barista: Compiling #{path}" }
-  c.on_compilation       { |path|         puts "Barista: Successfully compiled #{path}" }
-  c.on_compilation_error { |path, output| puts "Barista: Compilation of #{path} failed with:\n#{output}" }
-  c.on_compilation_with_warning { |path, output| puts "Barista: Compilation of #{path} had a warning:\n#{output}" }
+    c.before_compilation   { |path|         puts "Barista: Compiling #{path}" }
+    c.on_compilation       { |path|         puts "Barista: Successfully compiled #{path}" }
+    c.on_compilation_error { |path, output| puts "Barista: Compilation of #{path} failed with:\n#{output}" }
+    c.on_compilation_with_warning { |path, output| puts "Barista: Compilation of #{path} had a warning:\n#{output}" }
   
-  # Turn off preambles and exceptions on failure
+    # Turn off preambles and exceptions on failure
   
-  # c.verbose = false
+    # c.verbose = false
   
-  # Or, make sure it is always on
-  c.verbose!
+    # Or, make sure it is always on
+    c.verbose!
   
-  # If you want to use a custom JS file, you can as well
-  # e.g. vendoring CoffeeScript in your application:
-  # c.js_path = Rails.root.join('public', 'javascripts', 'coffee-script.js')
+    # If you want to use a custom JS file, you can as well
+    # e.g. vendoring CoffeeScript in your application:
+    # c.js_path = Rails.root.join('public', 'javascripts', 'coffee-script.js')
+  end
 end
