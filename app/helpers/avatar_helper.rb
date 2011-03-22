2167b4678ecc3ab0dfeedb6601f367c0de715c56
ericlemerdy <eric.lemerdy@laposte.net>
2011-03-22 01:34:51 +0100

    Design the session rendering (photo shadow) and css on title

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,7 +2,7 @@ require 'digest/md5'
 
 module AvatarHelper
   def avatar_for(user, options={})
-    image_tag(user.avatar.last, {:size => '64x64'}.merge(options))
+    image_tag(user.avatar.last, {:size => '64x64', :class => 'avatar'}.merge(options))
   end
   
   def avatar_hint(user)
