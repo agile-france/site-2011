04b5223bb8d3e623773196ec9f33f250faf2fe7c
thierry <thierry.henrio@gmail.com>
2011-05-25 21:28:24 +0200

    ouch mailer test were broken caveats : invoice attachment is not tested

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -4,7 +4,7 @@ class Mailer < ActionMailer::Base
 
   def invoice_ready(invoice, user=invoice.user)
     @invoice = invoice
-    attachments[invoice.pdf_filename] = File.read("#{Rails.public_path}#{invoice.pdf_url}")
+    attachments[invoice.pdf_filename] = File.read("#{Rails.public_path}#{invoice.pdf_url}") if invoice.pdf_url
     mail to: user.email, subject: "Votre facture #{invoice.ref} pour la Conférence Agile France 2011"
   end
 end
