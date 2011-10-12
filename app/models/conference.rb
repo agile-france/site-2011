621bb5a27b209c2e3c41ae57c918c656aeec2455
thierry <thierry.henrio@gmail.com>
2011-10-12 09:50:47 +0200

    a conference has registration engines a user can register to a conference using an engine link on conference page

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -8,7 +8,8 @@ class Conference
 
   field :name
   field :edition
-  attr_accessible :name, :edition
+  field :registration_engines, type: Array
+  attr_accessible :name, :edition, :registration_engines
   key :name, :edition
 
   # Public : new empty invoice for user
